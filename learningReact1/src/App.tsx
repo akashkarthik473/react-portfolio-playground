e78c7e90fb0e9fac849441765042a7fff16ffb43
Akash Karthik <akashkarthik473@gmail.com>
2025-05-29 11:42:47 -0700

    initial vite react template

---
+++ learningReact1/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyButton from './components/MyButton'
+function App() {
+
+
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  )
+}
+
+export default App
